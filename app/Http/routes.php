d0b6d22933a54a2a9855156c5b28f38f2e41e725
kibretbereket <kibret@archsoftwaresolutions.com>
2015-12-08 16:32:42 +0000

    kb_#109578630_agency
     five methods stubbed successfully

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,18 @@
 $app->get('/', function () use ($app) {
     return $app->welcome();
 });
+
+////Mapping of Agency controller
+
+$app->group(['prefix'=>'api/v1','namespace'=>'App\Http\Controllers'],function($app){
+
+//agency mapping
+ $app->post('agencies','AgencyController@create');                  //creates agencies
+ $app->get('agencies','AgencyController@retrieve');                 //retrieves the user
+ $app->get('agencies/{agency_id}','AgencyController@retrieveOne');    //retrieves one user
+ $app->put('agencies/{agency_id}','AgencyController@update') ;        //updates agencies
+ $app->delete('agencies/{agency_id}','AgencyController@delete');      //soft deletes agency
+
+
+
+});
