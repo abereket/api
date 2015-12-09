778b5e65b50ec6e83c94bc79a7141da22cd1fb6f
kibretbereket <kibret@archsoftwaresolutions.com>
2015-12-09 21:28:54 +0000

    kb_#109578638_users
     migration for users table

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateAgenciesTable' => $baseDir . '/database/migrations/2015_12_09_205032_create_agencies_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'ExampleTest' => $baseDir . '/tests/ExampleTest.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
