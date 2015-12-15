fad9ba4db3aa6c4fa7ee9428f3d7108b1ea8b06c
kibretbereket <kibret@archsoftwaresolutions.com>
2015-12-15 19:22:08 +0000

    team members
     stub five methods for users

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\JsonResponse;
 
 class Controller extends BaseController
 {
-    //
+    protected function buildFailedValidationResponse(Request $request, array $errors)
+    {
+            return new JsonResponse($errors, 422);
+
+    }
 }
