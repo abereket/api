8bc307d60cf2c3568ce021728ab777a85579406d
kibretbereket <kibret@archsoftwaresolutions.com>
2015-12-16 19:16:42 +0000

    kb_#109578654_team_member
    five Api's

--- database/migrations/2015_12_16_190620_create_team_members_table.php
+++ database/migrations/2015_12_16_190620_create_team_members_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeamMemberTable extends Migration
+class CreateTeamMembersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,9 +15,9 @@ class CreateTeamMemberTable extends Migration
         Schema::create('team_members', function (Blueprint $table) {
             $table->increments('id',11);
             $table->integer('team_id')->unsigned();
-            $table->foreign('team_id')->references('id')->on('teams');
+            $table->foreign('team_id')->references('id')->on('teams')->onUpdate('cascade');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
             $table->timestamps();
             $table->softDeletes()->nullable();
         });
