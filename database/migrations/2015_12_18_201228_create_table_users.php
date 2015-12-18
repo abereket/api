79c7de1265cd3087b0d79585bfbb96ae7596c502
kibretbereket <kibret@archsoftwaresolutions.com>
2015-12-18 20:46:12 +0000

    kb_#109578638_users
    migration

---
+++ database/migrations/2015_12_18_201228_create_table_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->binary('uuid',32);
+            $table->string('first_name',50);
+            $table->string('last_name',50);
+            $table->string('email',60);
+            $table->string('password',60);
+            $table->enum('type',['recruiter','candidate','agency','zemployee']);
+            $table->boolean('verified')->default(0);
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
