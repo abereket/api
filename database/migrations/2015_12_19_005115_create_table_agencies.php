5843d674d09025d781a6f2a768f1a6bf38da6b92
Amanuel Yohannes <eritreaTel@gmail.com>
2015-12-23 14:28:30 -0700

    fix problems with database migrations

--- database/migrations/2015_12_19_005115_create_table_agencies.php
+++ database/migrations/2015_12_19_005115_create_table_agencies.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUsers extends Migration
+class CreateTableAgencies extends Migration
 {
     /**
      * Run the migrations.
