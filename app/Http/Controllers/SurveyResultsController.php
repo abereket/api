02e5f35d945880e17bbbda2e8f4941c03b041e15
kibretbereket <kibret@archsoftwaresolutions.com>
2016-02-27 15:47:15 +0000

    [kb_#114395791_surveyResults_api]
    implementing survey-results api with its five and points

--- app/Http/Controllers/SurveyResultsController.php
+++ app/Http/Controllers/SurveyResultsController.php
@@ -11,7 +11,7 @@ class SurveyResultsController extends Controller{
      */
     public function create(Request $request){
         $rules = ['user_id'=>'required|integer','job_id'=>'required|integer','survey_id'=>'required|integer',
-                  'rating'=>'required|string|max:50', 'years_of_experience'=>'smallInteger'];
+                  'rating'=>'required|string|max:50', 'years_of_experience'=>'integer'];
         $this->validate($request,$rules);
 
         $surveyResultService = new SurveyResultsService();
@@ -26,16 +26,41 @@ class SurveyResultsController extends Controller{
      */
     public function update(Request $request,$id){
         $rules = ['user_id'=>'integer','job_id'=>'integer','survey_id'=>'integer',
-                  'rating'=>'string|max:50', 'years_of_experience'=>'smallInteger'];
+                  'rating'=>'string|max:50', 'years_of_experience'=>'Integer'];
         $this->validate($request,$rules);
 
         $surveyResultService = new SurveyResultsService();
         $surveyResult = $surveyResultService->update($request,$id);
         return response()->json($surveyResult);
     }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function retrieve(Request $request){
         $surveyResultService = new SurveyResultsService();
         $surveyResult = $surveyResultService->retrieve($request);
+        return response()->json($surveyResult);
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function retrieveOne($id){
+        $surveyResultService = new SurveyResultsService();
+        $surveyResult = $surveyResultService->retrieveOne($id);
+        return response()->json($surveyResult);
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function delete($id){
+        $surveyResultService = new SurveyResultsService();
+        $surveyResult = $surveyResultService->delete($id);
         return response()->json($surveyResult);
     }
 }
--- app/Services/SurveyResultsService.php
+++ app/Services/SurveyResultsService.php
@@ -17,7 +17,7 @@ class SurveyResultsService extends Base{
             return $valError;
         }
         $surveyResults = SurveyResults::create(['user_id'=>$request->json()->get('user_id'),'job_id'=>$request->json()->get('job_id'),
-                                               'survey_d'=>$request->json()->get('survey_id'),'rating'=>$request->json()->get('rating'),
+                                               'survey_id'=>$request->json()->get('survey_id'),'rating'=>$request->json()->get('rating'),
                                                'years_of_experience'=>$request->json()->get('years_of_experience')]);
         $surveyResults = $this->buildCreateSuccessMessage('success',$surveyResults);
         return $surveyResults;
@@ -47,8 +47,83 @@ class SurveyResultsService extends Base{
         return $surveyResult;
     }
 
-    public function retrieve(){
+    /**
+     * @param $request
+     * @return SurveyResults
+     */
+    public function retrieve($request){
+        $limit              =   ($request->input('per_page'))?($request->input('per_page')) : 15;
+        $userId             =   $request->input('user_id');
+        $jobId              =   $request->input('job_id');
+        $surveyId           =   $request->input('survey_id');
+        $rating             =   $request->input('rating');
+        $yearsOfExperience  =   $request->input('years_of_experience');
+        $createdAt          =   $request->input('created_at');
+        $updatedAt          =   $request->input('updated_at');
+        $deletedAt          =   $request->input('deleted_at');
+        $order_by           =   ($request->input('order_by'))? ($request->input('order_by')) : 'updated_at';
 
+        $surveyResult = new SurveyResults();
+        if ($userId) {
+            $surveyResult = $surveyResult->where('name' , 'like', '%'.$userId.'%');
+        }
+        if ($jobId) {
+            $surveyResult = $surveyResult->where('description',  'like', '%'.$jobId.'%');
+        }
+        if ($surveyId) {
+            $surveyResult = $surveyResult->where('name' , 'like', '%'.$surveyId.'%');
+        }
+        if ($rating) {
+            $surveyResult = $surveyResult->where('description',  'like', '%'.$rating.'%');
+        }
+        if ($yearsOfExperience) {
+            $surveyResult = $surveyResult->where('name' , 'like', '%'.$yearsOfExperience.'%');
+        }
+        if ($createdAt) {
+            $surveyResult = $surveyResult->where('description',  'like', '%'.$createdAt.'%');
+        }
+        if ($updatedAt) {
+            $surveyResult= $surveyResult->where('name' , 'like', '%'.$updatedAt.'%');
+        }
+        if ($deletedAt) {
+            $surveyResult = $surveyResult->where('description',  'like', '%'.$deletedAt.'%');
+        }
+        $surveyResult = $surveyResult->orderby($order_by)->paginate($limit);
+
+        $surveyResult= $this->buildRetrieveResponse($surveyResult->toArray());
+        $surveyResult= $this->buildRetrieveSuccessMessage("success",$surveyResult);
+        return $surveyResult;
+    }
+
+    /**
+     * @param $id
+     * @return array|string
+     */
+    public function retrieveOne($id){
+        $surveyResult =  SurveyResults::find($id);
+        $valError = $this->validateRetrieveOne($surveyResult);
+        if($valError){
+            $valError = $this->failureMessage($valError,parent::HTTP_404);
+            return $valError;
+        }
+        $surveyResult = $this->buildRetrieveOneSuccessMessage('success',$surveyResult);
+        return $surveyResult;
+    }
+
+    /**
+     * @param $id
+     * @return array|string
+     */
+    public function delete($id){
+        $surveyResult = SurveyResults::find($id);
+        $valError = $this->validateDelete($surveyResult);
+        if($valError){
+            $valError = $this->failureMessage($valError,parent::HTTP_404);
+            return $valError;
+        }
+        $surveyResult->delete();
+        $surveyResult = $this->buildDeleteSuccessMessage('success');
+        return $surveyResult;
     }
     /**
      * @param $userId
@@ -66,10 +141,10 @@ class SurveyResultsService extends Base{
         if(!$job){
             $errors = 'Please enter a valid job_id';
         }
-        //$survey = Surveys::find($surveyId);
-        //if(!$survey){
-           // $errors = 'Please enter a valid survey_id';
-        //}
+        $survey = Surveys::find($surveyId);
+        if(!$survey){
+            $errors = 'Please enter a valid survey_id';
+        }
         return $errors;
     }
 
@@ -97,12 +172,36 @@ class SurveyResultsService extends Base{
                 $errors = 'Please enter a valid job_id';
             }
         }
-       // if($surveyId || empty($surveyId)){
-           // $survey = Surveys::find($surveyId);
-            //if(!$survey){
-               // $errors = 'Please enter a valid survey_id';
-            //}
-       // }
+        if($surveyId || empty($surveyId)){
+           $survey = Surveys::find($surveyId);
+            if(!$survey){
+               $errors = 'Please enter a valid survey_id';
+            }
+        }
+        return $errors;
+    }
+
+    /**
+     * @param $surveyResult
+     * @return array|string
+     */
+    protected function validateRetrieveOne($surveyResult){
+        $errors = array();
+        if(!$surveyResult){
+            $errors = "The validate result you are looking is not found.Please enter a valid validate result id";
+        }
+        return $errors;
+    }
+
+    /**
+     * @param $surveyResult
+     * @return array|string
+     */
+    protected function validateDelete($surveyResult){
+        $errors = array();
+        if(!$surveyResult){
+            $errors = "The validate result you want to delete is not found.Please enter a valid validate result id";
+        }
         return $errors;
     }
 }
