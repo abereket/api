116900047f34094ca76f881787418fef4f279e5b
Amanuel Yohannes <eritreaTel@gmail.com>
2016-04-16 19:02:51 -0700

    modify routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -73,7 +73,7 @@ $app->group(['prefix'=>'api/v1','namespace'=>'App\Http\Controllers'],function($a
     $app->delete('survey-skills/{id}','SurveySkillsController@delete');             //deletes survey skills
 
     $app->post('authenticate','IsAuthenticatedController@authenticate');           //retrieves a user if authenticated
-    $app->delete('users','TestController@deleteUsers');                                 //deletes all users
+    $app->delete('delete-users','TestController@deleteUsers');                                 //deletes all users
 
     $app->post('documents','DocumentsController@create');                           //creates documents
     $app->get('documents/{id}','DocumentsController@retrieveOne');                  //retrieves one documents
