d592c335bba5e7acde1d2aeaf8bba96f13e33ada
Amanuel Yohannes <eritreaTel@gmail.com>
2016-05-02 21:40:09 -0700

    fix db_updates

--- database/migrations/2016_04_20_194629_update_documents_table.php
+++ database/migrations/2016_04_20_194629_update_documents_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateDocumentsTable extends Migration
+class UpdateDocumentsTableOne extends Migration
 {
     /**
      * Run the migrations.
